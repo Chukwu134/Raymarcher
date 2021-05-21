cbc1cb0576a9bda7d42d59aa46d6192f06a12f02
Evan Azari <eaa66@cornell.edu>
2021-05-20 22:37:31 -0400

    more plane animations

--- Rast/shaders/march.fs
+++ Rast/shaders/march.fs
@@ -15,24 +15,17 @@ out vec4 fragColor;
 
 const float PI = 3.14159265358979323846264;
 
-const float threshold = 0.001;
+const float threshold = 0.0005;
 
 vec4 worldPos = vec4(1);
 vec4 worldDir = vec4(0);
-
-int last = 0;
-vec3 xDir = vec3(1, 0, 0);
-vec3 yDir = vec3(0, 1, 0);
-vec3 zDir = vec3(0, 0, 1);
-float EPS = 0.0001;
-int Bailout = 50;
 
 // mandlebulb
 float distanceEstimatorMandlebulb(vec4 pos) {
   vec3 z = pos.xyz;
   float dr = 1.0;
   float r = 0.0;
-  int Power = 8;
+  int Power = 6;
 
   for(int i = 0; i < 50; i++) {
     r = length(z);
@@ -74,7 +67,7 @@ float distanceEstimatorCube(vec4 position) {
 }
 
 float get_glow(float minDistance) {
-  return 0.8 - 10 * minDistance;
+  return 0.8 - 40 * minDistance;
 }
 
 vec4 reflect(vec4 abcd, vec4 worldPt) {
@@ -95,34 +88,23 @@ void main() {
   vec4 pos = vec4(0, 0, 0, 1);
   worldPos = mVi * pos;
   worldDir = mVi * dir;
-  //worldPos = turnMat * worldPos;
-  //worldDir = turnMat * worldDir;
+
   float travelled = 0;
   float steps = 0;
   float distance = 100000;
   float minDistance = 100000;
 
   vec4 testingPos = worldPos;
-  //float theta = bias / 10;
-  vec3 planeDir = (turnMat * vec4(normalize(vec3(1, .5, .2)), 1)).xyz;
-  vec4 planeDirBias = vec4(planeDir, .3);
+  vec3 planeDir = (turnMat * vec4(vec3(1, 0, 0), 1)).xyz;
+  vec4 planeDirBias = vec4(planeDir, sin(theta) / 5 - 0.0);
   while(travelled < 50 && distance > threshold) {
-    // eyeSpace = inverse(mVi) * worldPos;
-    // if(eyeSpace.z > 5) {
-    //   eyeSpace.z = (eyeSpace.z - 5) * -1 + 5;
-    // }
-    // worldPos = mVi * eyeSpace;
-
-    // if(worldPos.z > 1) {
-    //   testingPos.z = (worldPos.z - 1) * -1 + 1;
-    // }
 
     vec4 turnedPos = turnMat * worldPos;
     testingPos = reflect(planeDirBias, turnedPos);
 
-    // testingPos = mod(worldPos + 1.5, 3) - 1.5;
-    // testingPos.y = mod(worldPos.y + 2, 5);
-    // testingPos.z = mod(worldPos.z + 2, 5);
+    //testingPos = mod(worldPos + 1.5, 3) - 1.5;
+    // testingPos.y = mod(worldPos.y + 1.5, 3) - 1.5;
+    // testingPos.z = mod(worldPos.z + 1.5, 3) - 1.5;
 
     distance = distanceEstimatorMandlebulb(testingPos);
     travelled += distance;
@@ -131,7 +113,7 @@ void main() {
     steps += 1;
   }
 
-  if(travelled < 10) {
+  if(travelled < 50) {
     vec3 color = vec3(length(testingPos.xyz) / 2, 0.2, .5);
     fragColor = vec4(color - steps / 200, 1);
     //fragColor = vec4(color, 1);
@@ -140,6 +122,7 @@ void main() {
     float glow = get_glow(minDistance);
     vec3 glow_hue = vec3(0.9, 1, 0.7);
 
-    fragColor = vec4(glow_hue * glow, 1);
+    //fragColor = vec4(glow_hue * glow, 0);
+    fragColor = vec4(0, 0, 0, 1);
   }
 }
